8e4cc4750f05ecdf5eb6bbcdc260e5dc8f9ce0e2
JohnAnnish <johnannish1234@gmail.com>
2026-03-10 23:39:31 +0530

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from src.analyzer import basic_analysis
 from src.complexity import check_complexity
@@ -18,17 +18,16 @@ if st.button("Analyze Code"):
     suggestions = basic_analysis(code)
 
     for s in suggestions:
-        st.write("- ", s)
+        st.write("-", s)
 
     st.subheader("Code Improvements")
     tips = improve_code(code)
 
     for t in tips:
-        st.write("- ", t)
+        st.write("-", t)
 
     st.subheader("Complexity Analysis")
     complexity = check_complexity(code)
 
     for c in complexity:
         st.write(f"Function: {c['name']} | Complexity: {c['complexity']}")
-
